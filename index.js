99356c6e7ad9a84a8aa260a457e301474757adc6
Jed Hunsaker <jed.hunsaker@gmail.com>
2013-06-05 00:38:19 -0700

    Fix sub-command issues

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+var path = require('path');
 var Command = require('./lib/Command');
-module.exports = new Command();
+
+
+var cmd = new Command(path.basename(process.argv[1]));
+
+cmd.on('error', function(err) {
+    console.log('');
+    console.log('  Error:', err.message);
+    cmd.outputUsage();
+    cmd.outputCommands();
+    cmd.outputOptions();
+    console.log();
+    process.exit(1);
+});
+
+module.exports = cmd;
